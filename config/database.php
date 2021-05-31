0f4df10db21cdd1705c30d9f89c35a02f069c707
Juan Cuellar <juanchokuellar@gmail.com>
2021-05-31 14:47:23 -0500

    Add Database Masterfile

--- config/database.php
+++ config/database.php
@@ -104,14 +104,14 @@ return [
             'prefix_indexes' => true,
         ],
 
-        'sqlsrv' => [
+        'sqlsrvmasterfile' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => '10.238.68.66\ADMIN',
+            'port' => '1434',
+            'database' => 'cp360',
+            'username' => 'sa',
+            'password' => 'D3v3l0p3r2121',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
