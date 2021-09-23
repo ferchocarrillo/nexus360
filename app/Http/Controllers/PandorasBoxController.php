2801e4369cfcce2a4c3d5882e523c63ab2daf3c1
Juan Cuellar <juanchokuellar@gmail.com>
2021-09-23 11:41:47 -0500

    PandorasBox Change Mails

--- app/Http/Controllers/PandorasBoxController.php
+++ app/Http/Controllers/PandorasBoxController.php
@@ -39,11 +39,10 @@ class PandorasBoxController extends Controller
             'created_by'=>Auth::user()->id
         ]);
         Mail::to([
-            'paula.arciniegas@cp-360.com',
-            'kelly.gamez@cp-360.com',
-            'katherine.olarte@cp-360.com',
-            'harold.penagos@cp-360.com',
-            'juand.cuellar@cp-360.com',
+            'kelly.gamez@contactpoint360.com',
+            'brigitte.pardo@contactpoint360.com',
+            'heidy.morales@contactpoint360.com',
+            'juand.cuellar@contactpoint360.com',
         ])->send(new PandorasBoxMail($pandora));
         
         return redirect()->back()->with('info', 'Created successfully');
