bdf4f6be9e81acd40c398944e32e5a026ff22dba
Juan Cuellar <juanchokuellar@gmail.com>
2022-06-09 07:02:59 -0500

    NPM run Prod, Disable devtools in production

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,9 @@ window.Vue = require('vue');
 
 Vue.config.productionTip = false;
 
+if (process.env.NODE_ENV === 'production') {
+    Vue.config.devtools = false;
+}
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
