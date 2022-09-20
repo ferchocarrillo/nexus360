d956849a647570c0dcfcc0faf5bf61a3a93f5ff3
Macro-JC <juanchokuellar@gmail.com>
2022-09-20 13:08:39 -0500

    American Water BO Tracker Add Queue and upgrades

--- database/seeds/AmericanWaterBoTrackerListsTableSeeder.php
+++ database/seeds/AmericanWaterBoTrackerListsTableSeeder.php
@@ -17,8 +17,90 @@ class AmericanWaterBoTrackerListsTableSeeder extends Seeder
         AmericanWaterBoTrackerList::create([
             'name' => 'queues',
             'list' => [
-                "AWRL Current Credits"=>["endForm"=>0],
-                "Problem Payment Issue"=>["endForm"=>1],
+                "AWRL Current Credits"=>[
+                    "fields"=>[
+                        "cus_id"=>true,
+                        "customer_name"=>true,
+                        "spreadsheet"=>true,
+                        "view"=>false,
+                        "status"=>true,
+                        "enr_number"=>true,
+                        "agreement_classification"=>true,
+                        "additional_notes"=>true,
+                    ]
+                ],
+                "Bulk refunds"=>[
+                    "fields"=>[
+                        "cus_id"=>true,
+                        "customer_name"=>false,
+                        "spreadsheet"=>false,
+                        "view"=>false,
+                        "status"=>false,
+                        "enr_number"=>false,
+                        "agreement_classification"=>false,
+                        "additional_notes"=>false,
+                    ]
+                ],
+                "Checklist"=>[
+                    "fields"=>[
+                        "cus_id"=>false,
+                        "customer_name"=>false,
+                        "spreadsheet"=>false,
+                        "view"=>true,
+                        "status"=>true,
+                        "enr_number"=>true,
+                        "agreement_classification"=>true,
+                        "additional_notes"=>true,
+                    ]
+                ],
+                "Journal Entries"=>[
+                    "fields"=>[
+                        "cus_id"=>false,
+                        "customer_name"=>false,
+                        "spreadsheet"=>false,
+                        "view"=>false,
+                        "status"=>false,
+                        "enr_number"=>false,
+                        "agreement_classification"=>false,
+                        "additional_notes"=>false,
+                    ],    
+                ],
+                "Problem Payment Issue"=>[
+                    "fields"=>[
+                        "cus_id"=>false,
+                        "customer_name"=>false,
+                        "spreadsheet"=>false,
+                        "view"=>false,
+                        "status"=>false,
+                        "enr_number"=>false,
+                        "agreement_classification"=>false,
+                        "additional_notes"=>false,
+                    ],    
+                ],
+                "Service Fees"=>[
+                    "fields"=>[
+                        "cus_id"=>false,
+                        "customer_name"=>false,
+                        "spreadsheet"=>false,
+                        "view"=>false,
+                        "status"=>false,
+                        "enr_number"=>false,
+                        "agreement_classification"=>false,
+                        "additional_notes"=>false,
+                    ],    
+                ],
+                "Settlements"=>[
+                    "fields"=>[
+                        "cus_id"=>false,
+                        "customer_name"=>false,
+                        "spreadsheet"=>false,
+                        "view"=>false,
+                        "status"=>false,
+                        "enr_number"=>false,
+                        "agreement_classification"=>false,
+                        "additional_notes"=>false,
+                    ],    
+                ],
             ]
         ]);
 
@@ -31,15 +113,40 @@ class AmericanWaterBoTrackerListsTableSeeder extends Seeder
                 "Pending - Claims"=>["gotoENR"=>0],
                 "On Hold - ENR"=>["gotoENR"=>1],
                 "On Hold - RM"=>["gotoENR"=>1],
+                "Solved"=>["gotoENR"=>1],
+                "ENR not in AX"=>["gotoENR"=>1],
+                "Not Solved"=>["gotoENR"=>1],
             ]
         ]);
 
         AmericanWaterBoTrackerList::create([
             'name' => 'agreement_classifications',
             'list' => [
+                'AWR Cancelled',
                 'Re-Enrollment',
                 'Renewal',
                 'New'
+            ]
+        ]);
+
+        AmericanWaterBoTrackerList::create([
+            'name' => 'views',
+            'list' => [
+                'Active ENR w/ End Date in Past',
+                'Active ENR w/ Incorrect Order Source',
+                'Active ENR w/ Invalid ENR Status Reason',
+                'Active ENR w/o Auto Renew Populated',
+                'Active ENR, DoNotAuto w Pending/Authorized Pmt Authorization',
+                'Cancel Off-Bill w/o Cancel Date',
+                'Cancelled w/ Invalid ENR Status Reason',
+                'Commercial Acct w. IHPP',
+                'Closed-Invalid sent to AX',
+                'Suspended w/ Invalid Payment Method',
+                'Suspended w/o Cancellation Date',
+                'Suspended w Cancel Date in Past',
+                'Suspended w/o Suspended Date',
+                'Missing Payment Report',
+                'AutoVoid Payment Authorization FallOut',
             ]
         ]);
     }
